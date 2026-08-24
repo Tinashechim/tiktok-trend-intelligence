8fb891a40f82e53e30199b5c1b40859b8e72c960
Tinashechim <chimanikire.tc@gmail.com>
2026-08-24 12:49:36 +0200

    Add admin panel for trend management

--- create_admin.py
+++ create_admin.py
@@ -1,4 +1,5 @@
-<!DOCTYPE html>
+﻿html_file = open('index.html', 'w', encoding='utf-8')
+html_file.write('''<!DOCTYPE html>
 <html>
 <head>
     <title>TrendPilot - TikTok Intelligence</title>
@@ -427,4 +428,6 @@
         loadTrends();
     </script>
 </body>
-</html>
+</html>''')
+html_file.close()
+print("Admin panel created!")
